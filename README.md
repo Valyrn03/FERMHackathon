b1fa1f0a6ecf793981185edf0c1252683a635f4c
valyrn <yoav.maoz0@gmail.com>
2025-09-20 13:19:18 -0400

    Progress :)

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 Backend will be written in Python, and send a string with the following structure:
 
 ### To Frontend
-HP\n
+    HP\n
     \tAbility (Text Descriptor)\n
     \tAbility (Text Descriptor)\n
     \tAbility (Text Descriptor)\n
-AI Ability (Text Descriptor)
+    \tActive Buffs <- list of strings
+    AI Ability (Text Descriptor)
+    \tActive AI Buffs <- list of strings
 ### From Frontend
 Ability
+
+# Rules
+Buffs will last for 3 rounds
--- game_logic/game.py
+++ game_logic/game.py
@@ -1,4 +1,22 @@
 # Handles the game loop
+from game_logic.player import Player
 
-def main_loop():
-    playerOne = Player
+player = Player()
+aiPlayer = Player()
+def initialize():
+    player = Player()
+    aiPlayer = Player()
+
+'''
+    HP\n
+    \tAbility (Text Descriptor)\n
+    \tAbility (Text Descriptor)\n
+    \tAbility (Text Descriptor)\n
+    \tActive Buffs <- list of strings
+    AI Ability (Text Descriptor)
+    \tActive AI Buffs <- list of strings
+'''
+def send_to_frontend() -> str:
+
+
+def receive_from_frontend(json_input: str) -> str:
