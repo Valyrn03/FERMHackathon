ddceed92253f61de4a750f616e0442efbaa0ae87
Ryan <ryan41410303@gmail.com>
2025-09-20 15:56:29 -0400

    getting the general layout of the screen

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -2,36 +2,50 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import fs from 'fs';
 
+function loadFile() {
+  fs.readFile('../state.txt', (error, data) => {
+    if (error) {
+      console.log('damn, an error');
+      console.log(error);
+      return;
+    }
 
+    console.log('got data');
+    console.log(data);
+  });
+}
+
+// write to the file and set the mutex so the back end can read it then write to this file
+// namely we will want to write the current state plus the move that was made
+function writeFile() {
+
+}
 
 function App() {
   const [count, setCount] = useState(0)
   const [playerHP, setPlayerHP] = useState(0)
   const [opponentHP, setOpponentHP] = useState(0)
 
+  const renderPlayerArea = (playerName, hitPoints) => {
+    return (
+      <div class="stat-bar">
+        <h2>{playerName}</h2>
+        <h4>{hitPoints}</h4>
+      </div>
+    )
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="game-area">
+        Insert game graphics here
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id="stat-bars-area">
+        {renderPlayerArea('The Player', playerHP)}
+        {renderPlayerArea('The AI Opponent', opponentHP)}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
