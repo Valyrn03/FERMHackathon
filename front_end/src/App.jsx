ba4be94cb6e4578d62c60f1d4c04f2252b9a5b2e
Ryan <ryan41410303@gmail.com>
2025-09-20 17:25:05 -0400

    showing abilities

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -4,7 +4,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import fs from 'fs';
 
-function loadFile() {
+
+function App() {
+  const [playerHP, setPlayerHP] = useState(0)
+  const [opponentHP, setOpponentHP] = useState(0)
+  const [abilities, setAbilities] = useState(['Punch', 'Kick', 'Run'])
+
+const readFile = () => {
   fs.readFile('../state.txt', (error, data) => {
     if (error) {
       console.log('damn, an error');
@@ -17,33 +23,41 @@ function loadFile() {
   });
 }
 
-// write to the file and set the mutex so the back end can read it then write to this file
+  // write to the file and set the mutex so the back end can read it then write to this file
 // namely we will want to write the current state plus the move that was made
-function writeFile() {
-
+const writeFile = (ability) => {
+  //const data = new Uint8Array(Buffer.from('Hello Node.js'));
+  //fs.writeFile('../state.txt',)
+  console.log(`used ${ability}`)
 }
 
-function App() {
-  const [count, setCount] = useState(0)
-  const [playerHP, setPlayerHP] = useState(0)
-  const [opponentHP, setOpponentHP] = useState(0)
-
-  const renderPlayerArea = (playerName, hitPoints) => {
+  const renderAbility = (ability) => {
     return (
-      <div class="stat-bar">
+      <div className='ability' onClick={() => writeFile(ability)}>{ability}</div>
+    )
+  }
+
+  const renderPlayerArea = (playerName, hitPoints, isHuman) => {
+    return (
+      <div className="stat-bar" onClick={() => writeFile()}>
         <h2>{playerName}</h2>
         <h4>{hitPoints}</h4>
+        {isHuman && (
+          <div id="abilities-wrapper">
+            {abilities.map(ability => renderAbility(ability))}
+          </div>
+        )}
       </div>
     )
   }
 
   return (
     <>
-      <div id="game-area">
+      <div id="game-area" onClick={() => readFile()}>
         Insert game graphics here
       </div>
       <div id="stat-bars-area">
-        {renderPlayerArea('The Player', playerHP)}
+        {renderPlayerArea('The Player', playerHP, true)}
         {renderPlayerArea('The AI Opponent', opponentHP)}
       </div>
     </>
