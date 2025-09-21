b2183543ee239dad10453116c202f3e8469f8e5e
Ryan <ryan41410303@gmail.com>
2025-09-21 09:59:49 -0400

    displaying buffs

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import player1 from './assets/Player-1.png'
 import theAI from './assets/The AI-1.png'
+import positiveAttack from './assets/+ATK-1.png.png'
+import positiveDefense from './assets/+DEF-1.png.png'
 import './App.css'
 
 
@@ -8,27 +10,39 @@ function App() {
   const [playerHP, setPlayerHP] = useState(100)
   const [opponentHP, setOpponentHP] = useState(100)
   const [abilities, setAbilities] = useState(['Punch', 'Kick', 'Run'])
+  const [playerAttackBuff, setPlayerAttackBuff] = useState(1)
+  const [playerDefenseBuf, setPlayerDefenseBuff] = useState(2)
+  const [aiAttackBuff, setAIAttackBuff] = useState(3)
+  const [aiDefenseBuff, setAIDefenseBuff] = useState(1)
 
-  // write to the file and set the mutex so the back end can read it then write to this file
-// namely we will want to write the current state plus the move that was made
-const writeFile = (ability) => {
-  //const data = new Uint8Array(Buffer.from('Hello Node.js'));
-  //fs.writeFile('../state.txt',)
+const useAbilityAndSendToServer = (ability) => {
   console.log(`used ${ability}`)
 }
 
   const renderAbility = (ability) => {
     return (
-      <input key={ability} type='button' className='ability' onClick={() => writeFile(ability)} value={ability}/>
+      <input key={ability} type='button' className='ability' onClick={() => useAbilityAndSendToServer(ability)} value={ability}/>
     )
   }
 
-  const renderPlayerArea = (playerName, hitPoints, imageSrc) => {
+  const renderBuffs = (numberOfBuffs, imgSrc) => {
+    const rows = [];
+
+    for (let idx = 0; idx < numberOfBuffs; idx += 1) {
+      rows.push(<img key={idx} height={50} width={50} src={imgSrc}/>)
+    }
+
+    return rows;
+  }
+
+  const renderPlayerArea = (playerName, hitPoints, imageSrc, attackBuff, defenseBuff) => {
     return (
       <div className="player-area">
         <h2>{playerName}</h2>
         <h2>{hitPoints}</h2>
         <img src={imageSrc} width={100} height={100} />
+        <div className='buff-wrapper'>{renderBuffs(attackBuff, positiveAttack)}</div>
+        <div className='buff-wrapper'>{renderBuffs(defenseBuff, positiveDefense)}</div>
       </div>
     )
   }
@@ -36,8 +50,8 @@ const writeFile = (ability) => {
   return (
     <>
       <div id="player-area-wrapper">
-        {renderPlayerArea('The Player', playerHP, player1)}
-        {renderPlayerArea('The AI Opponent', opponentHP, theAI)}
+        {renderPlayerArea('The Player', playerHP, player1, playerAttackBuff, playerDefenseBuf)}
+        {renderPlayerArea('The AI Opponent', opponentHP, theAI, aiAttackBuff, aiDefenseBuff)}
       </div>
       <div id="abilities-wrapper">
         {abilities.map(ability => renderAbility(ability))}
